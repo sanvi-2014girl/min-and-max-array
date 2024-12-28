6557b876662744002e89e13d1d1de562c3074a2e
sanvi-2014girl <goswamilipi82@gmail.com>
2024-12-28 11:46:56 +0530

    min and max array

---
+++ main.py
@@ -0,0 +1,14 @@
+def minElement(a, size):
+    temp = a[0]
+    for i in range(1,size):
+        temp = min(temp, a[i])
+    return temp
+def maxElement(a, size):
+    temp = a[0]
+    for i in range(1,size):
+        temp = max(temp, a[i])
+    return temp
+a = [12,1234,45,67,1]
+size = len(a)
+print("Minium element of array:",minElement(a,size))
+print("Maxium element of array:",maxElement(a,size))
